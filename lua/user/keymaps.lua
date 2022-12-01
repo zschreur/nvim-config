e40f77321437a8c0ec0396fa03eb2e53f0e6f71e
Zackary Schreur <z.schreur@techsmith.com>
2022-12-01 08:51:10 -0500

    keymaps

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,48 @@
+local opts = { silent = true }
+
+-- See h: key-notation
+
+--Remap space as leader key
+vim.keymap.set('', '<Space>', '<Nop>', opts)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+vim.keymap.set('n', '<leader>e', ':Lex 30<cr>', opts)
+
+-- Resize with arrows
+vim.keymap.set("n", "<M-Up>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<M-Down>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<M-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<M-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Insert --
+-- Press jk fast to enter
+vim.keymap.set("i", "jk", "<ESC>", opts)
+
+local term_opts = { remap = true, silent = true }
+
+-- Terminal --
+-- Better terminal navigation
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
