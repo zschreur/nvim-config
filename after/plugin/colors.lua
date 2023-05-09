b9467d6d23b1568e98ad4a00e8e64fe3252d1d76
Zackary Schreur <z.schreur@techsmith.com>
2023-05-09 09:29:08 -0400

    update colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,23 @@
 local defaultColor = "carbonfox"
 
+local currentColor = defaultColor
 function ColorMyPencils(color)
-    color = color or defaultColor
+   currentColor = color or defaultColor
 
-    vim.cmd.colorscheme(color)
+   vim.cmd.colorscheme(currentColor)
+end
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+local isTransparent = false;
+function ToggleTransparency()
+   if isTransparent then
+      ColorMyPencils(currentColor)
+   else
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+   end
+
+   isTransparent = not isTransparent
 end
 
 ColorMyPencils()
-
+-- ToggleTransparency()
