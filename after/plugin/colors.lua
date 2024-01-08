da5079be92e31e1a4061911fcd8a6cb67d349e0c
Zackary Schreur <z.schreur@techsmith.com>
2024-01-08 14:14:37 -0500

    update colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,23 +1,22 @@
 local defaultColor = "tokyonight-night"
 
 local currentColor = defaultColor
+local isTransparent = true;
+
 function ColorMyPencils(color)
    currentColor = color or defaultColor
 
    vim.cmd.colorscheme(currentColor)
-end
 
-local isTransparent = false;
-function ToggleTransparency()
-   if isTransparent then
-      ColorMyPencils(currentColor)
-   else
+   if (isTransparent) then
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
    end
+end
 
+function ToggleTransparency()
    isTransparent = not isTransparent
+   ColorMyPencils(currentColor)
 end
 
 ColorMyPencils()
--- ToggleTransparency()
