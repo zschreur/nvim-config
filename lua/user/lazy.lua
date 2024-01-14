13dfaf35dca918785fdfc958395e1203cde2eb67
Zackary Schreur <code@zackaryschreur.com>
2024-01-14 14:06:13 -0500

    add telescope to lazy nvim plugins

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,6 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-local plugins = {}
+local plugins = {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build =
+    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  }
+}
 local plugins = {}
 
 if not vim.loop.fs_stat(lazypath) then
