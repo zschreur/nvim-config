f9208ca6d6463c14df4deda53fc78a37846a1ca1
Zackary Schreur <z.schreur@techsmith.com>
2024-01-14 13:40:30 -0500

    modify telescope shortcuts

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,6 +6,5 @@ end
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<C-f>", builtin.grep_string, {})
-vim.keymap.set("n", "<leader>ps", function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end, {})
+vim.keymap.set("n", "<leader>pb", builtin.treesitter, {})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
