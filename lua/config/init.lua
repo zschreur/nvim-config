35b98c231496f3d0541695c24ae44be952944507
Zackary Schreur <z.schreur@techsmith.com>
2024-02-26 08:24:52 -0500

    Switch to lazy.nvim from packer

---
+++ lua/config/init.lua
@@ -0,0 +1,84 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local currentColor = "tokyonight-night"
+local isTransparent = true
+
+function ColorMyPencils(color)
+   currentColor = color
+
+   vim.cmd.colorscheme(currentColor)
+
+   if (isTransparent) then
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+   end
+end
+
+function ToggleTransparency()
+   isTransparent = not isTransparent
+   ColorMyPencils(currentColor)
+end
+
+local plugins = {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            ColorMyPencils("tokyonight-night")
+        end,
+    },
+    "EdenEast/nightfox.nvim",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            vim.cmd([[TSUpdate]])
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        }
+    },
+    "nvim-lualine/lualine.nvim",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    "ryanoasis/vim-devicons",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+
+    "hrsh7th/nvim-cmp",     -- completion plugin
+    "hrsh7th/cmp-buffer",   -- buffer completions
+    "hrsh7th/cmp-path",     -- path completions
+    "hrsh7th/cmp-cmdline",  -- nippet completions
+    "hrsh7th/cmp-nvim-lsp", -- lsp completions
+    "hrsh7th/cmp-nvim-lua", -- lua completions
+
+    -- lsp
+    "neovim/nvim-lspconfig",
+
+    -- tmux + vim navigation
+    -- use 'christoomey/vim-tmux-navigator'
+
+    -- CoPilot
+    "github/copilot.vim",
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
