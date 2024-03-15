30a8b3747cf998094f138162cc892b27de104cb4
Zackary Schreur <z.schreur@techsmith.com>
2024-03-15 11:02:43 -0400

    Add telescope-fzf-nativi as a telescope dependency

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -51,7 +51,11 @@ local plugins = {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
         dependencies = {
-            "nvim-lua/plenary.nvim"
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "cmake -S . -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+            }
         }
     },
     "nvim-lualine/lualine.nvim",
