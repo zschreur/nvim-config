4ab49f077c0e30c74d3f63892b5a31c78ac47ed2
Zackary Schreur <z.schreur@techsmith.com>
2024-03-30 20:51:55 -0400

    auto-format

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,23 +1,23 @@
-local builtin = require"telescope.builtin"
+local builtin = require "telescope.builtin"
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local utils = require "telescope.utils"
 local conf = require("telescope.config").values
 
 local set_opts_cwd = function(opts)
-  if opts.cwd then
-    opts.cwd = vim.fn.expand(opts.cwd)
-  else
-    opts.cwd = vim.loop.cwd()
-  end
+    if opts.cwd then
+        opts.cwd = vim.fn.expand(opts.cwd)
+    else
+        opts.cwd = vim.loop.cwd()
+    end
 
-  -- Find root of git directory and remove trailing newline characters
-  local jj_root, ret = utils.get_os_command_output({ "jj", "workspace", "root" }, opts.cwd)
-  local use_git_root = vim.F.if_nil(opts.use_git_root, true)
+    -- Find root of git directory and remove trailing newline characters
+    local jj_root, ret = utils.get_os_command_output({ "jj", "workspace", "root" }, opts.cwd)
+    local use_git_root = vim.F.if_nil(opts.use_git_root, true)
 
-  if ret==0 and use_git_root then
-    opts.cwd = jj_root[1]
-  end
+    if ret == 0 and use_git_root then
+        opts.cwd = jj_root[1]
+    end
 end
 
 local is_jj_repo = function()
@@ -26,15 +26,15 @@ local is_jj_repo = function()
 end
 
 local jj_files = function(opts)
-  opts = opts or {}
-  set_opts_cwd(opts)
-  pickers.new(opts, {
-    prompt_title = "Jujutsu Files",
-    finder = finders.new_table {
-      results = utils.get_os_command_output({ "jj", "files", "--no-pager" }, opts.cwd),
-    },
-    sorter = conf.generic_sorter(opts),
-  }):find()
+    opts = opts or {}
+    set_opts_cwd(opts)
+    pickers.new(opts, {
+        prompt_title = "Jujutsu Files",
+        finder = finders.new_table {
+            results = utils.get_os_command_output({ "jj", "files", "--no-pager" }, opts.cwd),
+        },
+        sorter = conf.generic_sorter(opts),
+    }):find()
 end
 
 local vcs_picker = function(opts)
