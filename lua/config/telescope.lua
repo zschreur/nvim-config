6391b80d3cf9ee994adaa737c25483a49c27f3b9
Zackary Schreur <z.schreur@techsmith.com>
2024-03-30 20:02:03 -0400

    Telescope: list jj files with ctrl-p

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,10 +1,22 @@
-local status_ok, builtin = pcall(require, "telescope.builtin")
-if not status_ok then
-    return
+local builtin = require"telescope.builtin"
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local utils = require "telescope.utils"
+local conf = require("telescope.config").values
+
+local jj_files = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "Jujutsu Files",
+    finder = finders.new_table {
+      results = utils.get_os_command_output({ "jj", "files", "--no-pager" }),
+    },
+    sorter = conf.generic_sorter(opts),
+  }):find()
 end
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<C-p>", jj_files, {})
 vim.keymap.set("n", "<C-f>", builtin.grep_string, {})
 vim.keymap.set("n", "<leader>pb", builtin.treesitter, {})
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
