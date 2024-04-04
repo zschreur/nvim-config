b19f3766330178e388a3324c832fc41da1c0e246
Zackary Schreur <z.schreur@techsmith.com>
2024-04-04 14:23:53 -0400

    Simplify vcf files telescope search

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,28 +1,24 @@
 local telescope = require "telescope"
 local builtin = require "telescope.builtin"
-local utils = require "telescope.utils"
 
 telescope.load_extension('jj')
 
-local is_jj_repo = function()
-    local _, ret = utils.get_os_command_output({ "jj", "root" })
-    return ret == 0
-end
-
 local vcs_picker = function(opts)
-    if is_jj_repo() then
-        return telescope.extensions.jj.pick(opts)
+    local jj_pick_status, jj_res = pcall(telescope.extensions.jj.files, opts);
+    if jj_pick_status then
+        return
     end
-    local status, res = pcall(builtin.git_files, opts);
 
-    if not status then
-        print(res)
+    local git_files_status, git_res = pcall(builtin.git_files, opts);
+    if not git_files_status then
+        print(jj_res)
+        print(git_res)
     end
 end
 
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", vcs_picker, {})
 vim.keymap.set("n", "<C-f>", builtin.grep_string, {})
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pb", builtin.treesitter, {})
 vim.keymap.set("n", "<leader>ps", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
