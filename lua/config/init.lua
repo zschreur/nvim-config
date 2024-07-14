7554ae8911c0c85757d5a04bef2c337fd53d9d9f
Zackary Schreur <code@zackaryschreur.com>
2024-07-14 07:46:46 -0400

    Add BurntSushi/ripgrep plugin as a telescope dependency
    `live_grep` and `grep_string` require this plugin

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -49,6 +49,7 @@ local plugins = {
         tag = "0.1.5",
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "BurntSushi/ripgrep",
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
                 build = "cmake -S . -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
